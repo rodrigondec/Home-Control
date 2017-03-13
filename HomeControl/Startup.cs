681cab02e30538b906f9c138d4034b40da36ec6d
Daniel Smith <danielsmithmxp@gmail.com>
2017-03-13 19:12:32 -0300

    Add project files.

---
+++ HomeControl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeControl.Startup))]
+namespace HomeControl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
