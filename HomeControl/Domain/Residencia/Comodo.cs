5c3b55c11844020ea68d03fec72c4478fef83526
Daniel Smith <danielsmithmxp@gmail.com>
2017-03-19 14:30:14 -0300

    Alterações no domínio

---
+++ HomeControl/Domain/Residencia/Comodo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeControl.Domain.Residencia
+{
+    public class Comodo
+    {
+    }
+}
---
+++ HomeControl/Domain/Residencia/Residencia.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeControl.Domain.Residencia
+{
+    public class Residencia
+    {
+        [Key]
+        private int id;
+        private String nome;
+        private HashSet<Comodo> comodos;
+
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                this.id = value;
+            }
+        }
+        public String Nome
+        {
+            get
+            {
+                return this.nome;
+            }
+            set
+            {
+                this.nome = value;
+            }
+        }
+        public HashSet<Comodo> Comodos
+        {
+            get
+            {
+                return this.comodos;
+            }
+            set
+            {
+                this.comodos = value;
+            }
+        }
+
+    }
+}
