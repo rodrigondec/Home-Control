aef65ba29bba105665d94549eba8380581a54db7
Daniel Smith <danielsmithmxp@gmail.com>
2017-03-22 16:17:57 -0300

    Inclusão de interface para o repositório.

---
+++ HomeControl/Dal/Repository/Base/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeControl.Dal.Repository.Base
+{
+    interface IRepository<T, ID> where T: class
+    {
+        List<T> GetAll();
+        T find(ID id);
+        T update(T entity);
+        void saveChanges();
+        T add(T entity);
+    }
+}
