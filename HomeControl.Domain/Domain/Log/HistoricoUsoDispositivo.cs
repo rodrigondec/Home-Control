fca9ceb0ef23c332567bd64bad9924d68f6b5e90
Daniel Smith <danielsmithmxp@gmail.com>
2017-04-11 23:51:24 -0300

    Historico de uso adicionado.

---
+++ HomeControl.Domain/Domain/Log/HistoricoUsoDispositivo.cs
@@ -0,0 +1,21 @@
+﻿using HomeControl.Domain.Dispositivos;
+using HomeControl.Domain.Domain.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeControl.Domain.Domain.Log
+{
+    public class HistoricoUsoDispositivo : IPersistable<int>
+    {
+
+        [Key]
+        public int Id { get; set;  }
+        public virtual Usuario Usuario { get; set; }
+        public virtual Dispositivo Dispositivo { get; set; }
+
+    }
+}
