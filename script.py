c1c6feb7d7824d11e869299126e086438efa6e28
RodrigoNdeC <rodrigondec@gmail.com>
2017-05-18 07:03:06 -0300

    script de teste adicionado

--- script.py
+++ script.py
@@ -1,7 +1,14 @@
 from app.models import *
 from app import db
 
-me = Usuario('rodrigo', 'rodrigondec@gmail.com', 'rodrigo123')
+obj = Usuario('rodrigo', 'rodrigondec@gmail.com', 'rodrigo123')
+obj2 = Sensor(13)
+obj3 = ModuloPrivado('quarto')
+obj4 = Modulo('jardim')
 
-db.session.add(me)
+# print(obj.tipo)
+db.session.add(obj)
+db.session.add(obj2)
+db.session.add(obj3)
+db.session.add(obj4)
 db.session.commit()
