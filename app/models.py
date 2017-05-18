c968f525ac78f60e3532e9dd9fa1349120554b88
RodrigoNdeC <rodrigondec@gmail.com>
2017-05-18 05:43:00 -0300

    models 'rodando'

--- app/models.py
+++ app/models.py
@@ -1,137 +1,158 @@
-class AbstractName(object):
-	def __init__(self, nome):
-		if self.__class__ is AbstractName:
-			raise TypeError('abstract class cannot be instantiated')
-		self.nome = nome
+from app import db
+from sqlalchemy.ext.declarative import declared_attr
 
-	@property
-	def nome(self):
-		return self.__nome
 
-	@nome.setter
-	def nome(self, nome):
-		self.__nome = nome
+# Many-to-many helper tables (for public access, use models only) -----------
 
-	def __str__(self):
-		return self.nome
+modulo_usuario = db.Table(
+    'modulo_usuario',
+    db.Column(
+        'id_usuario',
+        db.Integer,
+        db.ForeignKey('usuario.id_usuario')
+    ),
+    db.Column(
+        'id_modulo',
+        db.Integer,
+        db.ForeignKey('modulo_privado.id_modulo_privado')
+    )
+)
 
-class Client(object):
-	"""docstring for Client"""
-	def __init__(self, root):
-		if not(isinstance(root, Component)):
-			raise TypeError('Precisa ser um objeto do tipo Component')
-		self.root = root
+# modulo_component = db.Table(
+#     'modulo_component',
+#     db.Column(
+#         'id_modulo',
+#         db.Integer,
+#         db.ForeignKey('modulo.id_component')
+#     ),
+#     db.Column(
+#         'id_component',
+#         db.Integer,
+#         db.ForeignKey('component.id_component')
+#     )
+# )
 
-	def add_component(self, component):
-		self.root.add_component(component)
+# leaf_component = db.Table(
+#     'leaf_component',
+#     db.Column(
+#         'id_leaf',
+#         db.Integer,
+#         db.ForeignKey('leaf.id_leaf'),
+#         unique=True
+#     ),
+#     db.Column(
+#         'id_component',
+#         db.Integer,
+#         db.ForeignKey('component.id_component'),
+#         unique=True
+#     )
+# )
 
-	def __str__(self):
-		return self.root.nome
 
-class Usuario(AbstractName):
-	"""docstring for Usuario"""
-	def __init__(self, nome, email, senha):
-		super().__init__(nome)
-		self.email = email
-		self.senha = senha
-	
-	@property
-	def email(self):
-		return self.__email
+# Models and their simple relantionships -------------------------------------
 
-	@email.setter
-	def email(self, email):
-		self.__email = email
+class TemplateName(db.Model):
+    __abstract__ = True
 
-	@property
-	def senha(self):
-		return self.__senha
+    nome = db.Column(db.String(80))
 
-	@senha.setter
-	def senha(self, senha):
-		self.__senha = senha
+class Usuario(TemplateName):
+    id_usuario = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(64))
 
-class Embarcado(object):
-	"""docstring for Embarcado"""
-	def __init__(self, ip):
-		self.ip = ip
-		
-class Dispositivo(object):
-	"""docstring for Dispositivo"""
-	identificador=1
-	def __init__(self):
-		if self.__class__ is Dispositivo:
-			raise TypeError('abstract class cannot be instantiated')
-		self.identificador=Dispositivo.identificador
-		Dispositivo.identificador += 1
+class Client(db.Model):
+    id_client = db.Column(db.Integer, primary_key=True)
 
-class Sensor(Dispositivo):
-	"""docstring for Sensor"""
-	def __init__(self):
-		super().__init__()
-		self.arg = arg
-
-class Interruptor(Dispositivo):
-	"""docstring for Interruptor"""
-	def __init__(self, arg):
-		super().__init__()
-		self.arg = arg
-
-class Potenciometro(Dispositivo):
-	"""docstring for Potenciometro"""
-	def __init__(self, arg):
-		self.arg = arg
-
-class Uso(object):
-	"""docstring for Uso"""
-	def __init__(self, dispositivo=None, comando=None):
-		super(Uso, self).__init__()
-		self.dispositivo = dispositivo
-		self.comando = comando
-		
-class Monitor(AbstractName):
-	"""docstring for Monitor"""
-	def __init__(self, nome):
-		if self.__class__ is Monitor:
-			raise TypeError('abstract class cannot be instantiated')
-		super().__init__(nome)
-
-class Component(AbstractName):
-	"""docstring for Component"""
-	def __init__(self, nome):
-		super().__init__(nome)
-		if self.__class__ is Component:
-			raise TypeError('abstract class cannot be instantiated')
+class Component(TemplateName):
+	id_component = db.Column(db.Integer, primary_key=True)
+	tipo = db.Column(db.String(10))
+	# modulo_id = db.Column(db.Integer, db.ForeignKey('modulo.id_modulo'))
+	# @declared_attr
+	# def modulo_id(cls):
+	# 	return db.Column(db.Integer, db.ForeignKey('modulo.id_modulo'))
 
 class Leaf(Component):
-	"""docstring for Propriedade"""
-	def __init__(self, nome, embarcado=None, dispositivos=[], monitor=None):
-		super().__init__(nome)
-		self.embarcado = embarcado
-		self.dispositivos = dispositivos
-		self.monitor = monitor
+	id_leaf = db.Column(db.Integer, primary_key=True)
+	component_id = db.Column(db.Integer, db.ForeignKey('component.id_component'))
+	component = db.relationship("Component")
+	embarcado = db.relationship("Embarcado", uselist=False, back_populates="leaf")
+	dispositivos = db.relationship("Dispositivo", back_populates="leaf")
+	monitor = db.relationship("Monitor", uselist=False, back_populates="leaf")
 
 class Modulo(Component):
-	"""docstring for Modulo"""
-	def __init__(self, nome, components=[]):
-		if self.__class__ is Modulo:
-			raise TypeError('abstract class cannot be instantiated')
-		super().__init__(nome)
-		self.components = []
+	id_modulo = db.Column(db.Integer, primary_key=True)
+	component_id = db.Column(db.Integer, db.ForeignKey('component.id_component'))
+	component = db.relationship("Component")
+	components = db.relationship("Component", back_populates="modulo")
+	
 
-	def add_component(self, component):
-		if not(isinstance(component, Component)):
-			raise TypeError('Precisa ser um objeto do tipo Component')
-		self.components.append(component)
+	# @declared_attr
+	# def id_modulo(cls):
+	# 	return db.Column(db.Integer(), db.ForeignKey("component.id_component"), primary_key=True)
+	# @declared_attr
+	# def modulos(cls):
+	# 	return db.relationship("Component", back_populates="modulo")
 
-class ModuloPublico(Modulo):
-	"""docstring for ModuloPublico"""
-	def __init__(self, nome, components=[]):
-		super().__init__(nome, components)
+# class ModuloPublico(Modulo):
+# 	id_modulo_publico =  db.Column(db.Integer(), db.ForeignKey("modulo.id_modulo"), primary_key=True)
 
 class ModuloPrivado(Modulo):
-	"""docstring for ModuloPublico"""
-	def __init__(self, nome, components=[], usuarios=[]):
-		super().__init__(nome, components)
-		self.usuarios = usuarios
+	id_modulo_privado =  db.Column(db.Integer(), db.ForeignKey("modulo.id_modulo"), primary_key=True)
+	usuarios = db.relationship(
+        'Usuario',
+        secondary=modulo_usuario,
+        backref=db.backref('modulos', lazy='dynamic')
+    )
 
+class Embarcado(db.Model):
+	id_embarcado = db.Column(db.Integer, primary_key=True)
+	leaf_id = db.Column(db.Integer, db.ForeignKey('leaf.id_leaf'))
+	leaf = db.relationship("Leaf", back_populates="embarcado")
+
+class Dispositivo(db.Model):
+	id_dispositivo = db.Column(db.Integer, primary_key=True)
+	leaf_id = db.Column(db.Integer, db.ForeignKey('leaf.id_leaf'))
+	usos = db.relationship("Uso", back_populates="dispositivo")
+	tipo = db.Column(db.String(10))
+
+class Sensor(Dispositivo):
+	id_sensor =  db.Column(db.Integer, primary_key=True)
+	dispositivo_id = db.Column(db.Integer, db.ForeignKey('dispositivo.id_dispositivo'))
+	dispositivo = db.relationship("Dispositivo")
+
+class Interruptor(Dispositivo):
+	id_interruptor =  db.Column(db.Integer, primary_key=True)
+	dispositivo_id = db.Column(db.Integer, db.ForeignKey('dispositivo.id_dispositivo'))
+	dispositivo = db.relationship("Dispositivo")
+
+class Potenciometro(Dispositivo):
+	id_potenciometro =  db.Column(db.Integer, primary_key=True)
+	dispositivo_id = db.Column(db.Integer, db.ForeignKey('dispositivo.id_dispositivo'))
+	dispositivo = db.relationship("Dispositivo")
+
+class Uso(db.Model):
+	id_uso = db.Column(db.Integer, primary_key=True)
+	dispositivo_id = db.Column(db.Integer, db.ForeignKey('dispositivo.id_dispositivo'))
+	comando = db.Column(db.String(30))
+	hora = db.Column(db.DateTime, default=db.func.now())
+
+class Monitor(TemplateName):
+	id_monitor = db.Column(db.Integer, primary_key=True)
+	leaf_id = db.Column(db.Integer, db.ForeignKey('leaf.id_leaf'))
+	leaf = db.relationship("Leaf", back_populates="monitor")
+
+
+# class Base(db.Model):
+#     __abstract__ = True
+
+#     disabled = db.Column(db.Boolean, default=0)
+#     inserted_since = db.Column(db.DateTime, default=db.func.now())
+#     @declared_attr
+#     def inserted_by(cls):
+#         return db.Column(db.Integer, db.ForeignKey('user.id_user'))
+#     # inserted_by = db.Column(db.Integer, db.ForeignKey('user.id_user'))
+#     last_updated_since = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+#     @declared_attr
+#     def last_updated_by(cls):
+#         return db.Column(db.Integer, db.ForeignKey('user.id_user'))
