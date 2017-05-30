74a32543be0e5ce7e101e245c0f2eee74661c662
RodrigoNdeC <rodrigondec@gmail.com>
2017-05-30 02:35:44 -0300

    minor fix

--- app/models.py
+++ app/models.py
@@ -38,138 +38,142 @@ class TemplateStatus(db.Model):
     __abstract__ = True
     status = db.Column(db.String(30))
 
+    def __init__(self, status):
+        if self.__class__ is TemplateStatus:
+            raise TypeError('abstract class cannot be instantiated')
+        self.status = status
+
 
 class TemplateName(db.Model):
-	__abstract__ = True
+    __abstract__ = True
+    nome = db.Column(db.String(80))
 
-	nome = db.Column(db.String(80))
+    def __init__(self, nome):
+        if self.__class__ is TemplateName:
+            raise TypeError('abstract class cannot be instantiated')
+        self.nome = nome
 
-	def __init__(self, nome):
-		if self.__class__ is TemplateName:
-			raise TypeError('abstract class cannot be instantiated')
-		self.nome = nome
 
 class Usuario(TemplateName):
-	__tablename__ = 'usuario'
+    __tablename__ = 'usuario'
+    id_usuario = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True)
+    senha = db.Column(db.String(64))
 
-	id_usuario = db.Column(db.Integer, primary_key=True)
-	email = db.Column(db.String(50), unique=True)
-	senha = db.Column(db.String(64))
+    def __init__(self, nome, email, senha):
+        TemplateName.__init__(self, nome)
+        self.email = email
+        self.senha = senha
 
-	def __init__(self, nome, email, senha):
-		TemplateName.__init__(self, nome)
-		self.email = email
-		self.senha = senha
 
 class Client(db.Model):
-	__tablename__ = 'client'
+    __tablename__ = 'client'
+    id_client = db.Column(db.Integer, primary_key=True)
 
-	id_client = db.Column(db.Integer, primary_key=True)
 
 class Component(TemplateName):
-	__tablename__ = 'component'
+    __tablename__ = 'component'
+    id_component = db.Column(db.Integer, primary_key=True)
+    tipo = db.Column(db.String(15))
 
-	id_component = db.Column(db.Integer, primary_key=True)
-	tipo = db.Column(db.String(15))
+    def __init__(self, nome, tipo):
+        TemplateName.__init__(self, nome)
+        self.tipo = tipo
 
-	def __init__(self, nome, tipo):
-		TemplateName.__init__(self, nome)
-		self.tipo = tipo
 
 class Leaf(Component):
-	__tablename__ = 'leaf'
+    __tablename__ = 'leaf'
+    id_leaf = db.Column(db.Integer(), db.ForeignKey("component.id_component"), primary_key=True)
+    component = db.relationship("Component")
+    embarcado = db.relationship("Embarcado", uselist=False, back_populates="leaf")
+    dispositivos = db.relationship("Dispositivo", back_populates="leaf")
+    usos = db.relationship("Uso", back_populates="leaf")
+    monitor = db.relationship("Monitor", uselist=False, back_populates="leaf")
 
-	id_leaf = db.Column(db.Integer(), db.ForeignKey("component.id_component"), primary_key=True)
-	component = db.relationship("Component")
-	embarcado = db.relationship("Embarcado", uselist=False, back_populates="leaf")
-	dispositivos = db.relationship("Dispositivo", back_populates="leaf")
-	usos = db.relationship("Uso", back_populates="leaf")
-	monitor = db.relationship("Monitor", uselist=False, back_populates="leaf")
+    def __init__(self, nome):
+        Component.__init__(self, nome, self.__tablename__)
 
-	def __init__(self, nome):
-		Component.__init__(self, nome, self.__tablename__)
 
 class Modulo(Component):
-	__tablename__ = 'modulo'
-
-	id_modulo = db.Column(db.Integer(), db.ForeignKey("component.id_component"), primary_key=True)
-	component = db.relationship("Component", primaryjoin="and_(Modulo.id_modulo==Component.id_component)")
-	components = db.relationship(
+    __tablename__ = 'modulo'
+    id_modulo = db.Column(db.Integer(), db.ForeignKey("component.id_component"), primary_key=True)
+    component = db.relationship("Component", primaryjoin="and_(Modulo.id_modulo==Component.id_component)")
+    components = db.relationship(
         'Component',
         secondary=modulo_component,
         backref=db.backref('modulo', lazy='dynamic')
     )
 
-	def __init__(self, nome):
-		Component.__init__(self, nome, self.__tablename__)
+    def __init__(self, nome):
+        Component.__init__(self, nome, self.__tablename__)
+
 
 class ModuloPrivado(Modulo):
-	__tablename__ = 'modulo_privado'
-
-	id_modulo_privado =  db.Column(db.Integer(), db.ForeignKey("modulo.id_modulo"), primary_key=True)
-	modulo = db.relationship("Modulo", primaryjoin="and_(Modulo.id_modulo==Modulo_privado.id_modulo_privado)")
-	usuarios = db.relationship(
+    __tablename__ = 'modulo_privado'
+    id_modulo_privado = db.Column(db.Integer(), db.ForeignKey("modulo.id_modulo"), primary_key=True)
+    modulo = db.relationship("Modulo", primaryjoin="and_(Modulo.id_modulo==Modulo_privado.id_modulo_privado)")
+    usuarios = db.relationship(
         'Usuario',
         secondary=modulo_usuario,
         backref=db.backref('modulos', lazy='dynamic')
     )
 
-	def __init__(self, nome):
-		Component.__init__(self, nome, self.__tablename__)
+    def __init__(self, nome):
+        Component.__init__(self, nome, self.__tablename__)
+
 
 class Embarcado(db.Model):
-	__tablename__ = 'embarcado'
+    __tablename__ = 'embarcado'
+    id_embarcado = db.Column(db.Integer, primary_key=True)
+    leaf_id = db.Column(db.Integer, db.ForeignKey('leaf.id_leaf'))
+    leaf = db.relationship("Leaf", back_populates="embarcado")
+    ip = db.Column(db.String(15))
+    mac = db.Column(db.String(20))
 
-	id_embarcado = db.Column(db.Integer, primary_key=True)
-	leaf_id = db.Column(db.Integer, db.ForeignKey('leaf.id_leaf'))
-	leaf = db.relationship("Leaf", back_populates="embarcado")
-	ip = db.Column(db.String(15))
-	mac = db.Column(db.String(20))
+    def __init__(self, ip, mac):
+        self.ip = ip
+        self.mac = mac
 
-	def __init__(self, ip, mac):
-		self.ip = ip
-		self.mac = mac
 
 class Dispositivo(db.Model):
-	__tablename__ = 'dispositivo'
+    __tablename__ = 'dispositivo'
+    id_dispositivo = db.Column(db.Integer, primary_key=True)
+    porta = db.Column(db.Integer)
+    leaf_id = db.Column(db.Integer, db.ForeignKey('leaf.id_leaf'))
+    leaf = db.relationship("Leaf", back_populates="dispositivos")
+    usos = db.relationship("Uso", back_populates="dispositivo")
+    regras = db.relationship("Regra", back_populates="dispositivo")
+    tipo = db.Column(db.String(15))
 
-	id_dispositivo = db.Column(db.Integer, primary_key=True)
-	porta = db.Column(db.Integer)
-	leaf_id = db.Column(db.Integer, db.ForeignKey('leaf.id_leaf'))
-	leaf = db.relationship("Leaf", back_populates="dispositivos")
-	usos = db.relationship("Uso", back_populates="dispositivo")
-	tipo = db.Column(db.String(15))
+    def __init__(self, porta, tipo):
+        self.porta = porta
+        self.tipo = tipo
 
-	def __init__(self, porta, tipo):
-		self.porta = porta
-		self.tipo = tipo
 
 class Sensor(Dispositivo):
-	__tablename__ = 'sensor'
+    __tablename__ = 'sensor'
+    id_sensor = db.Column(db.Integer(), db.ForeignKey("dispositivo.id_dispositivo"), primary_key=True)
 
-	id_sensor =  db.Column(db.Integer(), db.ForeignKey("dispositivo.id_dispositivo"), primary_key=True)
+    def __init__(self, porta):
+        Dispositivo.__init__(self, porta, self.__tablename__)
 
-	def __init__(self, porta):
-		Dispositivo.__init__(self, porta, self.__tablename__)
 
 class Interruptor(Dispositivo):
-	__tablename__ = 'interruptor'
+    __tablename__ = 'interruptor'
+    id_interruptor = db.Column(db.Integer(), db.ForeignKey("dispositivo.id_dispositivo"), primary_key=True)
 
-	id_interruptor =  db.Column(db.Integer(), db.ForeignKey("dispositivo.id_dispositivo"), primary_key=True)
+    def __init__(self, porta):
+        Dispositivo.__init__(self, porta, self.__tablename__)
 
-	def __init__(self, porta):
-		Dispositivo.__init__(porta, self.__tablename__)
 
 class Potenciometro(Dispositivo):
-	__tablename__ = 'potenciometro'
+    __tablename__ = 'potenciometro'
+    id_potenciometro = db.Column(db.Integer(), db.ForeignKey("dispositivo.id_dispositivo"), primary_key=True)
 
-	id_potenciometro =  db.Column(db.Integer(), db.ForeignKey("dispositivo.id_dispositivo"), primary_key=True)
+    def __init__(self, porta):
+        Dispositivo.__init__(self, porta, self.__tablename__)
 
-	def __init__(self, porta):
-		Dispositivo.__init__(self, porta, self.__tablename__)
 
-class Uso(db.Model):
-	__tablename__ = 'uso'
 
 	id_uso = db.Column(db.Integer, primary_key=True)
 	leaf_id = db.Column(db.Integer, db.ForeignKey('leaf.id_leaf'))
