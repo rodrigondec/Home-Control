b416d261fa6cd02969d9fb72bcfc780d65dfd6e6
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-03 05:08:24 -0300

    script com uso adicionado

--- script.py
+++ script.py
@@ -6,41 +6,41 @@ from app import db
 client = Client.query.filter_by(id_client=1).first()
 
 # casa = ModuloPrivado('Casa rod')
-# db.session.add(casa)
 casa = ModuloPrivado.query.filter_by(id_modulo_privado=1).first()
 # client.component = casa
 
 # usuario = Usuario('rodrigo', 'rodrigondec@gmail.com', 'rodrigo123')
-# db.session.add(usuario)
 usuario = Usuario.query.filter_by(id_usuario=1).first()
 # casa.add_usuario(usuario)
 
 # quarto = ModuloPrivado('Quarto de rods')
-# db.session.add(quarto)
 quarto = ModuloPrivado.query.filter_by(id_modulo_privado=2).first()
 # casa.add_component(quarto)
 
 # leaf = Leaf('Dispositivos do quarto de rods')
-# db.session.add(leaf)
 leaf = Leaf.query.filter_by(id_leaf=3).first()
 # quarto.add_component(leaf)
 
 # embarcado = Embarcado('0.0.0.0', 'ex:abcd:efgh:ijkl')
-# db.session.add(embarcado)
 embarcado = Embarcado.query.filter_by(id_embarcado=1).first()
 # leaf.embarcado = embarcado
 
 # sensor = Sensor(13)
-# db.session.add(sensor)
 sensor = Sensor.query.filter_by(id_sensor=1).first()
 # leaf.add_dispositivo(sensor)
 
 # interruptor = Interruptor(12)
-# db.session.add(interruptor)
-interruptor = Interruptor.query.filter_by(id_interruptor=1).first()
+interruptor = Interruptor.query.filter_by(id_interruptor=2).first()
 # leaf.add_dispositivo(interruptor)
+# leaf.alterar_interruptor(interruptor, True)
 
-# @TODO adicionar uso
+# potenciometro = Potenciometro(11)
+potenciometro = Potenciometro.query.filter_by(id_potenciometro=3).first()
+# leaf.add_dispositivo(potenciometro)
+# leaf.alterar_potenciometro(potenciometro, 80.5)
+
+
+
 # @TODO adicionar regra
 # @TODO adicionar monitor
 
