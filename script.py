2c774a02ce7223170003fa777da1b3a5f2052927
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-04 15:37:38 -0300

    script com admin

--- script.py
+++ script.py
@@ -2,17 +2,24 @@ from app.models import *
 from app import db
 from datetime import datetime
 
-# client = Client()
+admin = Administrador('rodrigo', 'rodrigondec@gmail.com', 'rodrigo123')
+db.session.add(admin)
+# admin = Administrador.query.filter_by(id_administrador=1).first()
+
+client = Client()
 # db.session.add(client)
-client = Client.query.filter_by(id_client=1).first()
+# client = Client.query.filter_by(id_client=1).first()
+admin.client = client
 
-# casa = ModuloPrivado('Casa rod')
-casa = ModuloPrivado.query.filter_by(id_modulo_privado=1).first()
-# client.component = casa
+casa = ModuloPrivado('Casa rod')
+# casa = ModuloPrivado.query.filter_by(id_modulo_privado=1).first()
+client.component = casa
 
-# usuario = Usuario('rodrigo', 'rodrigondec@gmail.com', 'rodrigo123')
-usuario = Usuario.query.filter_by(id_usuario=1).first()
-# casa.add_usuario(usuario)
+usuario = Usuario('wesley', 'wereuelgmail.com', 'wesley123')
+# usuario = Usuario.query.filter_by(id_usuario=1).first()
+casa.add_usuario(usuario)
+casa.add_usuario(admin)
+
 
 # quarto = ModuloPrivado('Quarto de rods')
 quarto = ModuloPrivado.query.filter_by(id_modulo_privado=2).first()
