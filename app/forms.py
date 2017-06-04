f1f967f8e0ca5ea2c39b782f38fb0e4cf739282d
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-04 16:32:07 -0300

    forms adicionados

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, BooleanField, SelectField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     email = EmailField('email', validators=[DataRequired()])
     senha = PasswordField('senha', validators=[DataRequired()])
+
+class UsuarioForm(FlaskForm):
+    nome = StringField('nome', validators=[DataRequired()])
+    email = EmailField('email', validators=[DataRequired()])
+    senha = PasswordField('senha', validators=[DataRequired()])
+    is_admin = BooleanField('is_admin', default=False)
+
+class ClientForm(FlaskForm):
+    nome = StringField('nome', validators=[DataRequired()])
+
+class ComponentForm(FlaskForm):
+    modulo_pai = SelectField('modulo_pai', validators=[DataRequired()])
+    nome = StringField('nome', validators=[DataRequired()])
+    tipo_component = SelectField('tipo_component', validators=[DataRequired()])
+
+class DispositivoForm(FlaskForm):
+    leaf = SelectField('leaf', validators=[DataRequired()])
