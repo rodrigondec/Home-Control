edb390d624321e0f99a436613c2c1d25061f060b
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-04 22:00:42 -0300

    cadastrando propriedade ok

--- app/controllers/component.py
+++ app/controllers/component.py
@@ -1,6 +1,7 @@
 from flask import render_template, Blueprint, session, abort, flash, redirect, url_for
-from app import app
+from app import db
 from app.models import *
+from app.forms import ClientForm
 
 mod_component = Blueprint('component', __name__, url_prefix='/component', template_folder='templates')
 # @TODO fazer metodos controlador component
@@ -8,7 +9,31 @@ mod_component = Blueprint('component', __name__, url_prefix='/component', templa
 
 @mod_component.route('/')
 def index():
-    return "component index"
+    if 'logged_in' in session:
+        print(session['logged_in'])
+    return render_template('component/index.html')
+
+
+@mod_component.route('/propriedade/cadastrar', methods=['GET', 'POST'])
+def cadastrar_propriedade():
+    if 'logged_in' in session:
+        print(session['id_usuario'])
+        admin = Administrador.query.filter_by(id_administrador=session['id_usuario']).first()
+        if admin is not None and admin.client is None:
+            form = ClientForm()
+            if form.validate_on_submit():
+                admin.client = Client()
+                admin.client.component = ModuloPrivado(form.nome.data)
+
+                db.session.commit()
+
+                flash('Propriedade cadastrada')
+                return redirect(url_for('component.index'))
+            return render_template('component/cadastrar_propriedade.html', form=form)
+        else:
+            abort(403)
+    else:
+        abort(403)
 
 
 def listar_modulos():
