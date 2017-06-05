87f757a34a7b188054382f2d9be7e440d5c9d864
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-04 23:52:53 -0300

    listar_modulos funcionando

--- app/controllers/component.py
+++ app/controllers/component.py
@@ -35,10 +35,10 @@ def cadastrar_propriedade():
     else:
         abort(403)
 
-@mod_component.route('/component/listar_modulos')
+@mod_component.route('/listar_modulos')
 def listar_modulos():
-	components = Component.query.filter_by(tipo.like('modulo%')).all()
-	return render_template('component/listar_modulos.html', components=components)
+    components = Component.query.filter((Component.tipo=='modulo') | (Component.tipo=='modulo_privado')).all()
+    return render_template('component/listar_modulos.html', components=components)
 
 @mod_component.route('/cadastrar/<id_component_pai>')
 def cadastrar_component():
