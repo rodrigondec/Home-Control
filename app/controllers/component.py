15729f3717102863c0974f655f94092f25a55ff2
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-05 10:15:42 -0300

    listar_components =  index()

--- app/controllers/component.py
+++ app/controllers/component.py
@@ -7,11 +7,23 @@ mod_component = Blueprint('component', __name__, url_prefix='/component', templa
 # @TODO fazer metodos controlador component
 # @TODO fazer views component
 
+
 @mod_component.route('/')
-def index():
-    if 'logged_in' in session:
-        print(session['logged_in'])
-    return render_template('component/index.html')
+def listar_components():
+    components = Component.query.all()
+    return render_template('component/listar_components.html', components=components)
+
+
+@mod_component.route('/listar_components/<id_modulo>')
+def listar_components_por_modulo(id_modulo):
+    components = Component.query.filter((Component.tipo=='modulo') | (Component.tipo=='modulo_privado')).all()
+    return render_template('component/listar_components.html', components=components)
+
+
+@mod_component.route('/listar_dispositivos/<id_component>')
+def listar_dispositivos_por_leaf(id_component):
+    dispositivos = Dispositivo.query.filter_by(leaf_id=id_component)
+    return render_template('component/listar_dispositivos.html', dispositivos=dispositivos)
 
 
 @mod_component.route('/propriedade/cadastrar', methods=['GET', 'POST'])
@@ -39,24 +51,6 @@ def cadastrar_propriedade():
 @mod_component.route('/cadastrar/<id_component_pai>')
 def cadastrar_component():
     pass
-
-
-@mod_component.route('/listar_modulos')
-def listar_components():
-    components = Component.query.all()
-    return render_template('component/listar_components.html', components=components)
-
-
-@mod_component.route('/listar_modulos/<id_modulo>')
-def listar_components_por_modulo(id_modulo):
-    components = Component.query.filter((Component.tipo=='modulo') | (Component.tipo=='modulo_privado')).all()
-    return render_template('component/listar_components.html', components=components)
-
-
-@mod_component.route('/listar_dispositivos/<id_component>')
-def listar_dispositivos_por_leaf(id_component):
-    dispositivos = Dispositivo.query.filter_by(leaf_id=id_component)
-    return render_template('component/listar_dispositivos.html', dispositivos=dispositivos)
 
 
 def atualizar_dispositivo(id_dispositivo):
