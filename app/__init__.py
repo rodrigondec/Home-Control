72f7b0f80c017551189c8fe6adee1147e2d50295
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-04 22:32:20 -0300

    index do projeto redireciona para o login

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
@@ -26,6 +26,10 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+@app.route('/')
+def index():
+    return redirect('/dashboard/login')
 
 @app.errorhandler(404)
 def not_found(error):
