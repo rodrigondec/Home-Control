c56ef76572b5917b9169949e53464b0fbf1c46ff
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-10 00:26:07 -0300

    monitor funcionando

--- app/controllers/monitor.py
+++ app/controllers/monitor.py
@@ -1,4 +1,5 @@
 from flask import render_template, Blueprint, session, abort, flash, redirect, url_for
+from app.models import *
 from app.forms import MonitorForm
 
 mod_monitor = Blueprint('monitor', __name__, url_prefix='/monitor', template_folder='templates')
@@ -10,6 +11,13 @@ def monitor(id_leaf):
     if 'logged_in' in session:
         usuario = Usuario.query.filter_by(id_usuario=session['id_usuario']).first()
         leaf = Component.query.filter_by(id_component=id_leaf).first()
+        if not leaf.acessivel_por(usuario):
+            flash('Você não tem permissão para acessar o monitor desse leaf')
+            return redirect('/dashboard/')
+        if leaf.monitor is None:
+            flash('Cadastre um monitor')
+            return redirect('/monitor/cadastrar/'+id_leaf)
+        return render_template('monitor/monitor.html/', leaf=leaf)
     else:
         flash('Entre no sistema primeiro!')
         return redirect('/')
