cafd2479c32738133e0e1cca1b531b856ccbe0ca
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-10 00:47:39 -0300

    cadastrar regra iniciado

--- app/controllers/monitor.py
+++ app/controllers/monitor.py
@@ -3,8 +3,6 @@ from app.models import *
 from app.forms import MonitorForm
 
 mod_monitor = Blueprint('monitor', __name__, url_prefix='/monitor', template_folder='templates')
-# @TODO fazer metodos controlador monitor
-# @TODO fazer views monitor
 
 @mod_monitor.route('/<id_leaf>')
 def monitor(id_leaf):
@@ -24,7 +22,7 @@ def monitor(id_leaf):
 
 
 @mod_monitor.route('/cadastrar/<id_leaf>', methods=['GET', 'POST'])
-def cadastrar(id_leaf):
+def cadastrar_monitor(id_leaf):
     if 'logged_in' in session:
         usuario = Usuario.query.filter_by(id_usuario=session['id_usuario']).first()
         leaf = Component.query.filter_by(id_component=id_leaf).first()
@@ -44,6 +42,21 @@ def cadastrar(id_leaf):
 
             return redirect('/dashboard/leaf/'+id_leaf)
         return render_template('monitor/cadastrar_monitor.html', form=form)
+    else:
+        flash('Entre no sistema primeiro!')
+        return redirect('/')
+
+@mod_monitor.route('/cadastrar_regra/<id_monitor>', methods=['GET', 'POST'])
+def cadastrar_regra(id_monitor):
+    if 'logged_in' in session:
+        usuario = Usuario.query.filter_by(id_usuario=session['id_usuario']).first()
+        monitor = Monitor.query.filter_by(id_monitor=id_monitor).first()
+        if not monitor.leaf.alteravel_por(usuario):
+            flash('Você não tem permissão para cadastrar uma regra para esse monitor')
+            return redirect('/dashboard/')
+
+        # form = RegraForm()
+        return 'Cadastrar regra'
     else:
         flash('Entre no sistema primeiro!')
         return redirect('/')
