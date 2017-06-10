6c1a329c36723a84623525dd6d767982f034a3ae
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-10 19:06:36 -0300

    script funcionando

--- script.py
+++ script.py
@@ -8,18 +8,19 @@ db.session.add(AtualizarDispositivo())
 db.session.add(AlterarDispositivo())
 
 admin = Administrador('rodrigo', 'rodrigondec@gmail.com', 'rodrigo123')
+# admin = Usuario.query.filter_by(id_usuario=1).first()
 db.session.add(admin)
-# admin = Administrador.query.filter_by(id_usuario=1).first()
-
+#
+#
 client = Client()
-# db.session.add(client)
+db.session.add(client)
 # client = Client.query.filter_by(id_client=1).first()
 admin.client = client
-
+#
 casa = ModuloPrivado('Casa rod')
 # casa = ModuloPrivado.query.filter_by(id_component=1).first()
 client.component = casa
-
+#
 usuario = Usuario('wesley', 'wereuel@gmail.com', 'wesley123')
 # usuario = Usuario.query.filter_by(id_usuario=1).first()
 casa.add_usuario(usuario)
@@ -27,41 +28,50 @@ casa.add_usuario(admin)
 
 
 quarto = ModuloPrivado('Quarto de rods')
-# quarto = ModuloPrivado.query.filter_by(id_component=2).first()
+# quarto = Component.query.filter_by(id_component=2).first()
 quarto.add_usuario(admin)
 casa.add_component(quarto)
 #
 leaf = Leaf('Dispositivos do quarto de rods')
-# leaf = Leaf.query.filter_by(id_component=3).first()
+# leaf = Component.query.filter_by(id_component=3).first()
 quarto.add_component(leaf)
+
 
 embarcado = Embarcado('0.0.0.0', 'ex:abcd:efgh:ijkl')
 # embarcado = Embarcado.query.filter_by(id_embarcado=1).first()
 leaf.embarcado = embarcado
 #
 sensor = Sensor('Temperatura', 13)
-# sensor = Sensor.query.filter_by(id_dispositivo=1).first()
+# sensor = Dispositivo.query.filter_by(id_dispositivo=1).first()
 leaf.add_dispositivo(sensor)
 #
 interruptor = Interruptor('Lampâda central', 12)
-# interruptor = Interruptor.query.filter_by(id_dispositivo=2).first()
+# interruptor = Dispositivo.query.filter_by(id_dispositivo=2).first()
 leaf.add_dispositivo(interruptor)
 #
 potenciometro = Potenciometro('Ventilador', 11)
-# potenciometro = Potenciometro.query.filter_by(id_dispositivo=3).first()
+# potenciometro = Dispositivo.query.filter_by(id_dispositivo=3).first()
 leaf.add_dispositivo(potenciometro)
 #
+
 monitor = MonitorManual("custom")
-# monitor = MonitorHorario.query.filter_by(id_monitor=1).first()
+# monitor = Monitor.query.filter_by(id_monitor=1).first()
 leaf.monitor = monitor
 #
-regra = RegraCronometradaInterruptor(interruptor, True, datetime.now().hour, datetime.now().minute)
-# regra = RegraCronometradaInterruptor.query.filter_by(id_regra=1).first()
-monitor.add_regra(regra)
-
-regra2 = RegraInterruptor(interruptor, True)
-# regra2 = RegraCronometradaInterruptor.query.filter_by(id_regra=2).first()
+regra1 = RegraInterruptor(interruptor, True)
+# regra1 = Regra.query.filter_by(id_regra=1).first()
+monitor.add_regra(regra1)
+#
+regra2 = RegraInterruptorCronometrada(interruptor=interruptor, valor=True, hora=datetime.now().hour, minuto=datetime.now().minute)
+# regra2 = Regra.query.filter_by(id_regra=2).first()
 monitor.add_regra(regra2)
-
-
+#
+regra3 = RegraSensor(sensor=sensor, valor_inicial=10, valor_final=20, regra_atuadora=RegraPotenciometro(potenciometro=potenciometro, valor=0))
+# regra3 = Regra.query.filter_by(id_regra=4).first()
+monitor.add_regra(regra3)
+#
+regra4 = RegraSensorCronometrada(sensor=sensor, valor_inicial=10, valor_final=20, regra_atuadora=RegraPotenciometro(potenciometro=potenciometro, valor=3), hora=datetime.now().hour, minuto=datetime.now().minute)
+# regra4 = Regra.query.filter_by(id_regra=4).first()
+monitor.add_regra(regra4)
+#
 db.session.commit()
