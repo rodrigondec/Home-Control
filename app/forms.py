015bad9a299e5c7b88d7f41e08b4a36c73f436e2
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-10 19:06:01 -0300

    forms de regra adicionados

--- app/forms.py
+++ app/forms.py
@@ -43,3 +43,50 @@ class MonitorForm(FlaskForm):
     tipo_monitor = SelectField('Tipo Monitor',
                                  choices=[('MonitorManual', 'Monitor Manual'), ('MonitorAutomatico', 'Monitor Automático')])
 
+
+class RegraTipoDispositivoForm(FlaskForm):
+    form_name = HiddenField('regra_tipo_dispositivo')
+    tipo_dispositivo = SelectField('Tipo Dispositivo', validators=[DataRequired()], choices=[('Sensor', 'Sensor'), ('Interruptor', 'Interruptor'), ('Potenciometro', 'Potenciômetro')])
+
+
+class RegraInterruptorForm(FlaskForm):
+    form_name = HiddenField('regra_interruptor')
+    tipo_dispositivo = HiddenField('Tipo Dispositivo')
+    valor = BooleanField('Ligado')
+    cronometrado = BooleanField('Cronometrado', default=False)
+    hora = IntegerField('Hora', validators=[NumberRange(min=00, max=23)])
+    minuto = IntegerField('Minuto', validators=[NumberRange(min=00, max=60)])
+
+    def __init__(self, tipo_dispositivo):
+        super(RegraInterruptorForm, self).__init__()
+        self.tipo_dispositivo.data = tipo_dispositivo
+
+
+class RegraPotenciometroForm(FlaskForm):
+    form_name = HiddenField('regra_potenciometro')
+    tipo_dispositivo = HiddenField('Tipo Dispositivo')
+    valor = FloatField('Valor', validators=[DataRequired()])
+    cronometrado = BooleanField('Cronometrado', default=False)
+    hora = IntegerField('Hora', validators=[NumberRange(min=00, max=23)])
+    minuto = IntegerField('Minuto', validators=[NumberRange(min=00, max=60)])
+
+    def __init__(self, tipo_dispositivo):
+        super(RegraPotenciometroForm, self).__init__()
+        self.tipo_dispositivo.data = tipo_dispositivo
+
+
+class RegraSensorForm(FlaskForm):
+    form_name = HiddenField('regra_sensor')
+    tipo_dispositivo = HiddenField('Tipo Dispositivo')
+    valor_inicial = FloatField('Valor inicial', validators=[DataRequired()])
+    valor_final = FloatField('Valor inicial', validators=[DataRequired()])
+    atuador = SelectField('Atuador', validators=[DataRequired()], choices=[])
+    valor_atuador = StringField('Valor atuador', validators=[DataRequired()])
+    cronometrado = BooleanField('Cronometrado', default=False)
+    hora = IntegerField('Hora', validators=[NumberRange(min=00, max=23)])
+    minuto = IntegerField('Minuto', validators=[NumberRange(min=00, max=60)])
+
+    def __init__(self, tipo_dispositivo, leaf_id):
+        super(RegraSensorForm, self).__init__()
+        self.tipo_dispositivo.data = tipo_dispositivo
+        self.atuador.choices = Dispositivo.query.filter_by(leaf_id=leaf_id).filter((Dispositivo.tipo=='interruptor') | (Dispositivo.tipo=='potenciometro')).all()
