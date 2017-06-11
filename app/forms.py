a208f97b9afb30d1e1a20a1eeb00c01b1e820526
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-10 21:49:18 -0300

    labels alterados

--- app/forms.py
+++ app/forms.py
@@ -2,51 +2,52 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SelectField, IntegerField, FloatField, HiddenField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, NumberRange
+from app.models import Dispositivo
 
 
 class LoginForm(FlaskForm):
-    email = EmailField('email', validators=[DataRequired()])
-    senha = PasswordField('senha', validators=[DataRequired()])
+    email = EmailField(u'Email', validators=[DataRequired()])
+    senha = PasswordField(u'Senha', validators=[DataRequired()])
 
 
 class UsuarioForm(FlaskForm):
-    nome = StringField('nome', validators=[DataRequired()])
-    email = EmailField('email', validators=[DataRequired()])
-    senha = PasswordField('senha', validators=[DataRequired()])
-    is_admin = BooleanField('is_admin', default=False)
+    nome = StringField(u'Nome', validators=[DataRequired()])
+    email = EmailField(u'Email', validators=[DataRequired()])
+    senha = PasswordField(u'Senha', validators=[DataRequired()])
+    is_admin = BooleanField(u'Administrador', default=False)
 
 
 class ClientForm(FlaskForm):
-    nome = StringField('nome', validators=[DataRequired()])
+    nome = StringField(u'Nome', validators=[DataRequired()])
 
 
 class ComponentForm(FlaskForm):
-    nome = StringField('Nome', validators=[DataRequired()])
-    tipo_component = SelectField('Tipo Componente',
+    nome = StringField(u'Nome', validators=[DataRequired()])
+    tipo_component = SelectField(u'Tipo Componente',
                                  choices=[('Leaf', 'Folha'), ('Modulo', 'Módulo Público'), ('ModuloPrivado', 'Módulo Privado')])
 	
 
 class DispositivoForm(FlaskForm):
-    nome = StringField('Nome', validators=[DataRequired()])
-    porta = IntegerField('Porta', validators=[DataRequired()])
-    tipo_dispositivo = SelectField('Tipo Dispositivo',
+    nome = StringField(u'Nome', validators=[DataRequired()])
+    porta = IntegerField(u'Porta', validators=[DataRequired()])
+    tipo_dispositivo = SelectField(u'Tipo Dispositivo',
                                  choices=[('Sensor', 'Sensor'), ('Interruptor', 'Interruptor'), ('Potenciometro', 'Potenciômetro')])
 
 
 class EmbarcadoForm(FlaskForm):
-    ip = StringField('Pp', validators=[DataRequired()])
-    mac = StringField('Mac', validators=[DataRequired()])
+    ip = StringField(u'Ip', validators=[DataRequired()])
+    mac = StringField(u'Mac', validators=[DataRequired()])
 
 
 class MonitorForm(FlaskForm):
     nome = StringField('Nome', validators=[DataRequired()])
-    tipo_monitor = SelectField('Tipo Monitor',
+    tipo_monitor = SelectField(u'Tipo Monitor',
                                  choices=[('MonitorManual', 'Monitor Manual'), ('MonitorAutomatico', 'Monitor Automático')])
 
 
 class RegraTipoDispositivoForm(FlaskForm):
-    form_name = HiddenField('regra_tipo_dispositivo')
-    tipo_dispositivo = SelectField('Tipo Dispositivo', validators=[DataRequired()], choices=[('Sensor', 'Sensor'), ('Interruptor', 'Interruptor'), ('Potenciometro', 'Potenciômetro')])
+    form_name = HiddenField('regra_tipo_dispositivo', default='regra_tipo_dispositivo')
+    tipo_dispositivo = SelectField(u'Tipo Dispositivo', validators=[DataRequired()], choices=[('Sensor', 'Sensor'), ('Interruptor', 'Interruptor'), ('Potenciometro', 'Potenciômetro')])
 
 
 class RegraInterruptorForm(FlaskForm):
