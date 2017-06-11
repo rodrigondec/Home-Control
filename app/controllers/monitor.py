fba37d045514a1f41f03ecb68b2435d9a40ef184
RodrigoNdeC <rodrigondec@gmail.com>
2017-06-10 21:50:17 -0300

    cadastrar regra quase lá

--- app/controllers/monitor.py
+++ app/controllers/monitor.py
@@ -1,6 +1,6 @@
-from flask import render_template, Blueprint, session, abort, flash, redirect, url_for
+from flask import render_template, Blueprint, session, abort, flash, redirect, url_for, request
 from app.models import *
-from app.forms import MonitorForm
+from app.forms import MonitorForm, RegraTipoDispositivoForm, RegraSensorForm, RegraPotenciometroForm, RegraInterruptorForm
 
 mod_monitor = Blueprint('monitor', __name__, url_prefix='/monitor', template_folder='templates')
 
@@ -47,7 +47,7 @@ def cadastrar_monitor(id_leaf):
         return redirect('/')
 
 @mod_monitor.route('/cadastrar_regra/<id_monitor>', methods=['GET', 'POST'])
-def cadastrar_regra(id_monitor):
+def regra_tipo_dispositivo(id_monitor):
     if 'logged_in' in session:
         usuario = Usuario.query.filter_by(id_usuario=session['id_usuario']).first()
         monitor = Monitor.query.filter_by(id_monitor=id_monitor).first()
@@ -55,8 +55,38 @@ def cadastrar_regra(id_monitor):
             flash('Você não tem permissão para cadastrar uma regra para esse monitor')
             return redirect('/dashboard/')
 
-        # form = RegraForm()
-        return 'Cadastrar regra'
+        form = RegraTipoDispositivoForm()
+        if form.validate_on_submit():
+            return redirect('/monitor/cadastrar_regra/'+id_monitor+'/'+form.tipo_dispositivo.data)
+        return render_template('monitor/regra_tipo_dispositivo.html', form=form)
+    else:
+        flash('Entre no sistema primeiro!')
+        return redirect('/')
+
+@mod_monitor.route('/cadastrar_regra/<id_monitor>/<tipo_dispositivo>', methods=['GET', 'POST'])
+def regra_dispositivo(id_monitor, tipo_dispositivo):
+    if 'logged_in' in session:
+        usuario = Usuario.query.filter_by(id_usuario=session['id_usuario']).first()
+        monitor = Monitor.query.filter_by(id_monitor=id_monitor).first()
+        if not monitor.leaf.alteravel_por(usuario):
+            flash('Você não tem permissão para cadastrar uma regra para esse monitor')
+            return redirect('/dashboard/')
+
+        if tipo_dispositivo == 'Sensor':
+            form = RegraSensorForm(tipo_dispositivo, monitor.leaf_id)
+            if form.validate_on_submit():
+                pass
+            return render_template('monitor/regra_sensor.html', form=form)
+        else:
+            if tipo_dispositivo == 'Interruptor':
+                form = RegraInterruptorForm(tipo_dispositivo, monitor.leaf_id)
+            else:
+                form = RegraPotenciometroForm(tipo_dispositivo, monitor.leaf_id)
+            if form.validate_on_submit():
+                pass
+            return render_template('monitor/regra_int_pot.html', form=form)
+
+
     else:
         flash('Entre no sistema primeiro!')
         return redirect('/')
